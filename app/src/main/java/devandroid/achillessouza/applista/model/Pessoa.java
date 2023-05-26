6a637bf627ff443985c14883b6347ab80498203c
akillez01 <77950840+akillez01@users.noreply.github.com>
2023-05-26 01:38:28 -0300

    Metodo toString - POO Herança Polimorfismo

--- app/src/main/java/devandroid/achillessouza/applista/model/Pessoa.java
+++ app/src/main/java/devandroid/achillessouza/applista/model/Pessoa.java
@@ -1,4 +1,61 @@
 package devandroid.achillessouza.applista.model;
 
+
+//OrdemDePagamento - pascalCase - CamelCase
 public class Pessoa {
+
+    // Contrutor
+    // Atributos - Objetos - Model - Modelo - template
+    // Métodos de Acesso - Getts and Sets
+
+    private String PrimeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+    private String telefoneContato;
+
+    public Pessoa(){}
+
+    public String getPrimeiroNome() {
+        return PrimeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+
+       this.PrimeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "PrimeiroNome='" + PrimeiroNome + '\'' +
+                ", sobreNome='" + sobreNome + '\'' +
+                ", cursoDesejado='" + cursoDesejado + '\'' +
+                ", telefoneContato='" + telefoneContato + '\'' +
+                '}';
+    }
 }
+// To String - Para Textos - Para Strings de Caracteres
--- app/src/main/java/devandroid/achillessouza/applista/view/MainActivity.java
+++ app/src/main/java/devandroid/achillessouza/applista/view/MainActivity.java
@@ -5,12 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import devandroid.achillessouza.applista.R;
+import devandroid.achillessouza.applista.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
+
+    Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String getDadosPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pessoa = new Pessoa();
+        // Atribuir conteúdo, dados, valores para o Objeto
+        // Comforme o seu MODELO, TEMPLATE
+        pessoa.setPrimeiroNome("Achilles");
+        pessoa.setSobreNome("Souza");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("92-993742692");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome(" Luiz ");
+        outraPessoa.setSobreNome(" Alves ");
+        outraPessoa.setCursoDesejado(" Java ");
+        outraPessoa.setTelefoneContato("92-999999999");
+
+        dadosPessoa = " Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+
+        int parada = 0;
+
+
     }
 }
